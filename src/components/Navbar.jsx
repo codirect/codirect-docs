2a3fd2ed6c4f2c1b4062baa005c6766b208f07ef
Preslav Kolev <contact@codirect.live>
2026-06-17 00:55:37 +0300

    Added discord link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,7 +128,7 @@ export default function Navbar({ onOpenAiOverlay }) {
       <div className="navbar-right">
         <div className="navbar-icons">
           <a href="https://github.com/codirect/codirect" target='_blank' aria-label="GitHub" className="icon-link"><FaGithub size={20} /></a>
-          <a href="#" aria-label="Discord" className="icon-link"><FaDiscord size={20} /></a>
+          <a href="https://discord.gg/TVh2bbYx" target='_blank' aria-label="Discord" className="icon-link"><FaDiscord size={20} /></a>
           <a href="https://codirect.live" aria-label="Main Website" target='_blank' className="icon-link"><Globe size={20} /></a>
         </div>
       </div>
